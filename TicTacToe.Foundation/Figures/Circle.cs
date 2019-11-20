99b2634e8d14757d32d686c2e2c01850dc741aed
andrew-nasevich <49556147+andrew-nasevich@users.noreply.github.com>
2019-11-20 12:44:58 +0300

    Figure infrastructure was implemented (#1)

---
+++ TicTacToe.Foundation/Figures/Circle.cs
@@ -0,0 +1,15 @@
+﻿using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Foundation.Figures
+{
+    public class Circle : IFigure
+    {
+        public FigureType Type { get; }
+
+
+        public Circle()
+        {
+            Type = FigureType.Circle;
+        }
+    }
+}
---
+++ TicTacToe.Foundation/Figures/Cross.cs
@@ -0,0 +1,15 @@
+﻿using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Foundation.Figures
+{
+    public class Cross : IFigure
+    {
+        public FigureType Type { get; }
+
+
+        public Cross()
+        {
+            Type = FigureType.Cross;
+        }
+    }
+}
