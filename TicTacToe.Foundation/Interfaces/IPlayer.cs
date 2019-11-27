dd53bf86ed9f3807fc6c898a34630f902ff77578
Andrew <nasevichandrey@gmail.com>
2019-11-27 11:34:08 +0300

    IPlayer and Player were implemented

---
+++ TicTacToe.Foundation/Interfaces/IPlayer.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe.Foundation.Interfaces
+{
+    public interface IPlayer
+    {
+        IFigure Figure { get; }
+
+        string Name { get; }
+
+        int Id { get; }
+    }
+}
---
+++ TicTacToe.Foundation/Players/Player.cs
@@ -0,0 +1,21 @@
+﻿using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Foundation.Players
+{
+    public class Player : IPlayer
+    {
+        public IFigure Figure { get; }
+
+        public string Name { get; }
+
+        public int Id { get; }
+
+
+        public Player(IFigure figure, string name, int id)
+        {
+            Figure = figure;
+            Name = name;
+            Id = id;
+        }
+    }
+}
