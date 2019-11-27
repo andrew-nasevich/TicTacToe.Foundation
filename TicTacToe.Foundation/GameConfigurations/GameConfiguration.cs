937c4deb1f2bce7f3c35960c0373db4418ad8bd6
Andrew <nasevichandrey@gmail.com>
2019-11-27 11:51:03 +0300

    IGameConfiguration and GameConfiguration were implemented

---
+++ TicTacToe.Foundation/GameConfigurations/GameConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Foundation.GameConfigurations
+{
+    public class GameConfiguration : IGameConfiguration
+    {
+        public IReadOnlyCollection<IPlayer> Players { get; }
+
+        public IPlayer FirstStepPlayer { get; }
+
+        public int BoardSize { get; }
+
+
+        public GameConfiguration(IReadOnlyCollection<IPlayer> players, IPlayer firstStepPlayer, int boardSize)
+        {
+            Players = players;
+            FirstStepPlayer = firstStepPlayer;
+            BoardSize = boardSize;
+        }
+    }
+}
---
+++ TicTacToe.Foundation/Interfaces/IGameConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToe.Foundation.Interfaces
+{
+    public interface IGameConfiguration
+    {
+        IReadOnlyCollection<IPlayer> Players { get; }
+
+        IPlayer FirstStepPlayer { get; }
+
+        int BoardSize { get; }
+    }
+}
