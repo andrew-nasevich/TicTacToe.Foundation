ec97c6e385eddd0ed81b1f21f3e87e26e920c88f
andrew-nasevich <49556147+andrew-nasevich@users.noreply.github.com>
2019-12-03 12:38:13 +0300

    GameConfiguration infrastructure was implemented (#6)
    * IPlayer and Player were implemented
    
    * IGameConfiguration and GameConfiguration were implemented
    
    * Properties were reordered. Id property was removed
    
    * Properties were reordered
    
    * Params in Player were reordered

---
+++ TicTacToe.Foundation/GameConfigurations/GameConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Foundation.GameConfigurations
+{
+    public class GameConfiguration : IGameConfiguration
+    {
+        public int BoardSize { get; }
+
+        public IPlayer FirstStepPlayer { get; }
+
+        public IReadOnlyCollection<IPlayer> Players { get; }
+
+
+        public GameConfiguration(int boardSize, IPlayer firstStepPlayer, IReadOnlyCollection<IPlayer> players)
+        {
+            BoardSize = boardSize;
+            FirstStepPlayer = firstStepPlayer;
+            Players = players;
+        }
+    }
+}
---
+++ TicTacToe.Foundation/Interfaces/IGameConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToe.Foundation.Interfaces
+{
+    public interface IGameConfiguration
+    {
+        int BoardSize { get; }
+
+        IPlayer FirstStepPlayer { get; }
+
+        IReadOnlyCollection<IPlayer> Players { get; }
+    }
+}
