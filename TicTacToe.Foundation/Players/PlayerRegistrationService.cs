0dc4412a301e12ae408ff2c07514b9a8827e6afe
Andrew <nasevichandrey@gmail.com>
2019-12-03 10:49:45 +0300

    player registration service development was started

---
+++ TicTacToe.Foundation/Players/PlayerRegistrationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Foundation.Interfaces;
+using TicTacToe.Foundation.Figures;
+
+namespace TicTacToe.Foundation.Players
+{
+    public class PlayerRegistrationService
+    {
+        private IEnumerable<IPlayer> _players;
+
+        private IEnumerable<FigureType> _allFigureTypes;
+        public PlayerRegistrationService(IEnumerable<FigureType> allFigureTypes)
+        {
+            
+        }
+        
+    }
+}
