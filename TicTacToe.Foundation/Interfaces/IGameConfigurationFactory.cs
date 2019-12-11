5d5f585c9f835a49b6473230186294254141cd37
Andrew <nasevichandrey@gmail.com>
2019-12-11 10:20:36 +0300

    GameConfigurationFactory was implemented

---
+++ TicTacToe.Foundation/Interfaces/IGameConfigurationFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToe.Foundation.Interfaces
+{
+    public interface IGameConfigurationFactory
+    {
+        IGameConfiguration CreateGameConfiguration(int boardSize, IPlayer firstStepPlayer, IReadOnlyCollection<IPlayer> players);
+    }
+}
