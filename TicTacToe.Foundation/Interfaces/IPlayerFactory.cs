ecf9d9d2328c1fd4012533e35d01c19f2d07cb1b
andrew-nasevich <49556147+andrew-nasevich@users.noreply.github.com>
2019-12-12 12:38:24 +0300

    player registration service  (#8)

---
+++ TicTacToe.Foundation/Interfaces/IPlayerFactory.cs
@@ -0,0 +1,9 @@
+﻿using TicTacToe.Foundation.Figures;
+
+namespace TicTacToe.Foundation.Interfaces
+{
+    public interface IPlayerFactory
+    {
+        IPlayer CreatePlayer(string name, FigureType figureType);
+    }
+}
---
+++ TicTacToe.Foundation/Players/PlayerFactory.cs
@@ -0,0 +1,13 @@
+﻿using TicTacToe.Foundation.Figures;
+using TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Foundation.Players
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string name, FigureType figureType)
+        {
+            return new Player(name, figureType);
+        }
+    }
+}
